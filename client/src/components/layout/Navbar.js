4e13c4da198787b4b6df750bc84b4afb6b2046bf
Marcus Peh <hkmpeh@gmail.com>
2021-05-21 14:02:07 +0800

    change navbar to show only when logged in

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,40 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class Navbar extends Component {
     render() {
-        return (
-        <div className="navbar-fixed">
-            <nav className="z-depth-0">
-            <div className="nav-wrapper white">
-                <Link
-                to="/"
-                style={{
-                    fontFamily: "monospace"
-                }}
-                className="col s5 brand-logo center black-text"
-                >
-                <i className="material-icons">code</i>
-                MERN
-                </Link>
-            </div>
-            </nav>
-        </div>
-        );
+        if (this.props.auth.isAuthenticated) 
+            return (
+                <div className="navbar-fixed">
+                    <nav className="z-depth-0">
+                    <div className="nav-wrapper white">
+                        <Link
+                        to="/"
+                        style={{
+                            fontFamily: "monospace"
+                        }}
+                        className="col s5 brand-logo center black-text"
+                        >
+                        <i className="material-icons">code</i>
+                        MERN
+                        </Link>
+                    </div>
+                    </nav>
+                </div>
+            );
+        else 
+            return <></>;
     }
 }
-export default Navbar;
+
+Navbar.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(Navbar);
