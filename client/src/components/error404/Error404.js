248dc6295c5e1710c5d60880e975ab425cfddc48
Marcus Peh <hkmpeh@gmail.com>
2021-05-21 22:32:20 +0800

    styled error 404 page

--- client/src/components/error404/Error404.js
+++ client/src/components/error404/Error404.js
@@ -4,18 +4,26 @@ import { Link } from "react-router-dom";
 class Error404 extends Component {
     render() {
         return (
-            <div className="center">
-                <h1>404</h1>
-                <p>It looks like the page doesn't exist - please check the URL and try again.</p>
-                <Link to="/"
-                style={{
-                  width: "180px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }} className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-                    Take me home
-                </Link>
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+            <div className="row">
+                <div className="col s12 center-align">
+                    <h1>404</h1>
+                    <p className="flow-text grey-text text-darken-1">
+                        It looks like the page doesn't exist :( 
+                        <br/>
+                        Please check the URL and try again
+                    </p>
+                    <Link to="/"
+                        style={{
+                        width: "180px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px"
+                        }} className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                        Take me home
+                    </Link>
+                </div>
             </div>
+        </div>
         );
     }
 }
