c0c4c08defcbfd3e9085ec38d5c4851eb695f846
Marcus Peh <hkmpeh@gmail.com>
2021-06-12 11:09:50 +0800

    Updated layout of dashboard and sales

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 class Dashboard extends Component {
     render() {
         return (
-            <>
-                <h1>Dashboard</h1>
-                <p>To be developed</p>
-            </>
+            <Container style={{marginTop:"1rem"}}>
+                <Row>
+                    <Col xs={2} style={{textAlign:"center"}}>
+                        <h5>Total Revenue</h5>
+                    </Col>
+                    <Col xs={2} style={{textAlign:"center"}}>
+                        <h5>Daily Revenue</h5>
+                    </Col>
+                    <Col xs={2} style={{textAlign:"center"}}>
+                        <h5>Average sold</h5>
+                    </Col>
+                    <Col xs={6} >
+                        <h5>Notification</h5>
+                    </Col>
+                    
+                    <Col xs={2} style={{textAlign:"center", height: "20vh"}}>
+                        <p style={{marginTop: "1rem", fontStyle: "italic"}}>$0</p>
+                    </Col>
+                    <Col xs={2} style={{textAlign:"center", height: "20vh"}}>
+                        <p style={{marginTop: "1rem", fontStyle: "italic"}}>$0</p>
+                    </Col>
+                    <Col xs={2} style={{textAlign:"center", height: "20vh"}}>
+                        <p style={{marginTop: "1rem", fontStyle: "italic"}}>$0</p>
+                    </Col>
+                    <Col xs={6} style={{height: "20vh"}}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis libero vel magna porta, quis luctus purus sagittis. In bibendum tincidunt bibendum. In accumsan risus justo, at ullamcorper felis commodo sit amet. Sed risus metus, aliquam at sem interdum, cursus vulputate lectus. Orci varius natoque penatibus et magnis dis parturient.</p>
+                    </Col>
+                </Row>
+                <Row style={{height: "30vh"}}>
+                    <Col xs={12}>
+                        <h5>Chart</h5>
+                        <p>Use chart.js to plot graph</p>
+                    </Col>
+                </Row>
+                <Row style={{height: "30vh"}}>
+                    <Col xs={12}>
+                        <h5>Transaction</h5>
+                        <p>Just print out a table of Transaction</p>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
--- client/src/components/sale/Sale.js
+++ client/src/components/sale/Sale.js
@@ -1,12 +1,72 @@
 import React, { Component } from "react";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 class Sales extends Component {
     render() {
         return (
-            <>
-                <h1>Sales</h1>
-                <p>To be developed</p>
-            </>
+            <Container style={{marginTop:"1rem"}}>
+                <Row>
+                    <Col xs={9} style={{height: "60vh"}}>
+                        <Row>
+                            <Col xs={12} xs={12} style={{height: "30vh"}}>
+                                <h5>Chart</h5>
+                                <p>Use chart.js to generate graph</p>
+                            </Col>
+                            <Col xs={4} style={{height: "30vh"}}>
+                                <h5>Revenue</h5>
+                                <p>Use chart.js to generate graph</p>
+                            </Col>
+                            <Col xs={4} style={{height: "30vh"}}>
+                                <h5>Revenue per month</h5>
+                                <p>Use chart.js to generate graph</p>
+                            </Col>
+                            <Col xs={4} style={{height: "30vh"}}>
+                                <h5>Sales per day</h5>
+                                <p>Use chart.js to generate graph</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs={3} style={{height: "60vh"}}>
+                        <Row style={{height: "20vh"}}>
+                            <Col xs={12}>
+                                <span style={{fontSize:"1.5em"}}>$0</span>
+                                <p>Revenue (all time)</p>
+                                <span style={{fontSize:"1.5em"}}>$0</span>
+                                <p>Profit (all time)</p>
+                            </Col>
+                        </Row>
+                        
+                        <Row style={{height: "20vh"}}>
+                            <Col xs={12}>
+                                <span style={{fontSize:"1.5em"}}>$0</span>
+                                <p>Revenue (year to date)</p>
+                                <span style={{fontSize:"1.5em"}}>$0</span>
+                                <p>Revenue (year to date)</p>
+                            </Col>
+                        </Row>
+                        
+                        <Row style={{height: "20vh"}}>
+                            <Col xs={12}>
+                                <span style={{fontSize:"1.5em"}}>$0</span>
+                                <p>Revenue(monthly)</p>
+                                <span style={{fontSize:"1.5em"}}>$0</span>
+                                <p>Profit (monthly)</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs={6} style={{height: "30vh"}}>
+                        <h5>Top sales</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis libero vel magna porta, quis luctus purus sagittis. In bibendum tincidunt bibendum. In accumsan risus justo, at ullamcorper felis commodo sit amet. </p>
+                    </Col>
+                    <Col xs={6} style={{height: "30vh"}}>
+                        <h5>Worse sale</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis libero vel magna porta, quis luctus purus sagittis. In bibendum tincidunt bibendum. In accumsan risus justo, at ullamcorper felis commodo sit amet.</p>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
