a2e0f2f6875050fbf10a67f56433997f07da9e74
Marcus Peh <hkmpeh@gmail.com>
2021-07-19 20:46:00 +0800

    added in extra test for register page

--- cypress/integration/registerTest/unauthenticatedRegister.test.js
+++ cypress/integration/registerTest/unauthenticatedRegister.test.js
@@ -6,14 +6,10 @@ describe('Unauthenticated Landing Page Test', () => {
         cy.then(() => {
             window.localStorage.clear()
         })
-        cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000/register')
     })
 
     it('Register page looks good', () => {
-        // check if register page loads
-        cy.log('Checking if register page loads')
-        cy.visit('http://localhost:3000/register')  
-
         // check if link to login page works
         cy.log('Checking if link to log in page works')
         cy.contains('Already have an account? Log in').should('exist').contains('Log in').click()
@@ -35,4 +31,55 @@ describe('Unauthenticated Landing Page Test', () => {
         cy.log('Checking if form to register an account exist')
         cy.get('form')
     })
+
+    it('Registering without filling in the form', () => {
+        // Clearing the form and registering
+        cy.log('Testing for error when nothing is filled in')
+        cy.get('[id=name]').clear()
+        cy.get('[id=email]').clear()
+        cy.get('[id=password]').clear()
+        cy.get('[id=password2]').clear()
+        cy.contains('REGISTER').click()
+        cy.contains('Name field is required').should('exist')
+        cy.contains('Email field is required').should('exist')
+        cy.contains('Password must be at least 6 characters').should('exist')
+        cy.contains('Confirm password field is required').should('exist')
+    })
+
+    it('Registering with invalid email', () => {
+        // Registering with invalid email
+        cy.log('Testing for error when invalid email is entered')
+        cy.get('[id=email]').clear().type("asdfghjkl")
+        cy.contains('REGISTER').click()
+        cy.contains('Email is invalid').should('exist')
+    })
+
+    it('Registering with short password', () => {
+        // Registering with short password
+        cy.log('Testing for error when invalid email is entered')
+        cy.get('[id=password]').clear().type("12345")
+        cy.contains('REGISTER').click()
+        cy.contains('Password must be at least 6 characters').should('exist')
+    })
+
+    it('Registering with mismatch second password', () => {
+        // Registering with mismatch second password
+        cy.log('Testing for error when password confirmation field is mismatch')
+        cy.get('[id=password]').clear().type("asdfghjkl")
+        cy.get('[id=password2]').clear().type("asdfghjk")
+        cy.contains('REGISTER').click()
+        cy.contains('Passwords must match').should('exist')
+    })
+
+    it('Registering with registered email', () => {
+        // Registering with mismatch second password
+        cy.log('Testing for error when email entered is registered')
+        cy.get('[id=name]').clear().type("markeet")
+        cy.get('[id=email]').clear().type("test@markeet.com")
+        cy.get('[id=password]').clear().type("asdfghjkl")
+        cy.get('[id=password2]').clear().type("asdfghjkl")
+        cy.contains('REGISTER').click()
+        cy.contains('Email already exists').should('exist')
+    })
+
 })
