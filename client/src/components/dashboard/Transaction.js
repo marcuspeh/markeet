8bcda973c5e20586d6d48779fad60feccbbfb377
Kar Hee <eekarheenicholas@gmail.com>
2021-07-25 14:38:01 +0800

    reverted back to original without space in tax

--- client/src/components/dashboard/Transaction.js
+++ client/src/components/dashboard/Transaction.js
@@ -39,7 +39,6 @@ class Transaction extends Component {
     }
   }
 
-<<<<<<< HEAD
   render() {
     var dateTime = this.props.transaction.date.substring(0, 19).split("T");
     const taxAmount =
@@ -112,80 +111,6 @@ class Transaction extends Component {
                       <td>{transaction.quantity}</td>
                       <td>${transaction.price}</td>
                       <td>${transaction.price * transaction.quantity}</td>
-=======
-    render() {
-        var dateTime = this.props.transaction.date.substring(0, 19).split("T");
-        const taxAmount = Math.round((this.props.transaction.total * this.state.tax) * 100) / 100;
-        return (
-            <>
-            <Container>
-                <Row>
-                    <Col>
-                        <div style={{float:"left", margin:"1em"}}>
-                        <button type="button"  className="close" onClick={this.onSubmit}>
-                            <i className="material-icons left">keyboard_backspace</i> Back to dashboard
-                        </button>
-                        </div>
-                    </Col>
-                </Row>
-                <br />
-                <div style={{textAlign: "center"}}>
-                    <h3>{this.state.name}</h3>
-                    <p>{this.state.address ? "Address: " + this.state.address : ""}</p> 
-                    <p>{this.state.number ? "Telephone: " + this.state.number : ""}</p> 
-                </div>
-                <hr style={{border: "none", borderTop: "3px double #333",height: "5px"}} />
-                <p><b>Transaction ID:</b> {this.props.transaction._id}</p>
-                <p><b>Date:</b> {dateTime[0]}</p>
-                <p><b>Time:</b> {dateTime[1]}</p>
-                <hr style={{border: "none", borderTop: "3px double #333",height: "5px"}} />
-                
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Barcode</th>
-                            <th>Title</th>
-                            <th>Quantity</th>
-                            <th>Unit Price</th>
-                            <th>Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {this.props.transaction && this.props.transaction.cartItems.length ? Array.from(this.props.transaction.cartItems)
-                        .map((transaction) => (
-                            <tr key={transaction._id}>
-                                <td>{transaction.barcode}</td>
-                                <td>{transaction.title}</td>
-                                <td>{transaction.quantity}</td>
-                                <td>${transaction.price}</td>
-                                <td>${transaction.price * transaction.quantity}</td>
-                            </tr>
-                        )): (
-                        <tr>
-                            <td colSpan="5">Nothing Here :)</td>
-                        </tr>
-                        )}
-                    <tr style={{border:"none"}}>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td style={{textAlign: "right"}}><b>Subtotal</b>:</td>
-                        <td>${this.props.transaction.total}</td>
-                    </tr>
-                    <tr style={{border:"none"}}>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td style={{textAlign: "right", paddingTop: "0px"}}><b>Tax({Math.round(this.state.tax * 100 *10) / 10}%)</b>:</td>
-                        <td style={{paddingTop: "0px"}}>${taxAmount}</td>
-                    </tr>
-                    <tr style={{border:"none"}}>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td style={{textAlign: "right", paddingTop: "0px"}}><b>Total:</b></td>
-                        <td style={{paddingTop: "0px"}}>${Math.round((this.props.transaction.total + taxAmount) * 100) / 100}</td>
->>>>>>> marcus
                     </tr>
                   )
                 )
@@ -208,7 +133,7 @@ class Transaction extends Component {
                 <td></td>
                 <td></td>
                 <td style={{ textAlign: "right", paddingTop: "0px" }}>
-                  <b>Tax ({Math.round(this.state.tax * 100 * 10) / 10}%)</b>:
+                  <b>Tax({Math.round(this.state.tax * 100 * 10) / 10}%)</b>:
                 </td>
                 <td style={{ paddingTop: "0px" }}>${taxAmount}</td>
               </tr>
